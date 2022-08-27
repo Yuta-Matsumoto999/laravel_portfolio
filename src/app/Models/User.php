ea32d1e76a3471a4cccb2d6ab0a673e565891d51
Yuta-Matsumoto999 <y.matsumoto0723@gmail.com>
2022-08-27 22:45:33 +0900

    add verify email

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -2,25 +2,39 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
+use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmailContract
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, MustVerifyEmail, Notifiable;
 
     /**
      * The attributes that are mass assignable.
      *
      * @var array<int, string>
      */
+    use SoftDeletes;
+
     protected $fillable = [
-        'name',
+        'user_name',
         'email',
         'password',
+        'twitter_name',
+        'twitter_icon',
+        'line_name',
+        'line_id',
+        'line_icon',
+        'google_name',
+        'self_introduction',
+        'birthday',
+        'sports',
+        'user_kind'
     ];
 
     /**
